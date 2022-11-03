c11461941fc69bfe95d160d9b7f332a7b1efb6d4
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2022-11-03 08:32:08 +0100

    Dodanie klasy LineSegment

---
+++ 2D-Game/LineSegment.cpp
@@ -0,0 +1,26 @@
+#include "LineSegment.h"
+
+LineSegment::LineSegment() {}
+
+LineSegment::LineSegment(Point2D startPoint, Point2D endPoint) {
+	//TODO write after implementing = operator overloading
+}
+
+LineSegment::LineSegment(int x1, int y1, int x2, int y2) {
+	startPoint.setPoint(x1, y1);
+	endPoint.setPoint(x2, y2);
+}
+
+Point2D LineSegment::getStartPoint() {
+	return this->startPoint;
+}
+Point2D LineSegment::getEndPoint() {
+	return this->endPoint;
+}
+
+void LineSegment::setStartPoint(Point2D point) {
+	this->startPoint = point;
+}
+void LineSegment::setEndPoint(Point2D point) {
+	this->endPoint = point;
+}
