8166386659755abc81076bc169e2e051869109b8
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2022-11-18 12:44:44 +0100

    Rozszerzenie klasy Point2D o możliwość wykonywania przekształceń geometrycznych

--- 2D-Game/Point2D.cpp
+++ 2D-Game/Point2D.cpp
@@ -1,39 +1,76 @@
 #include "Point2D.h"
 
 Point2D::Point2D() {
-	x = 0;
-	y = 0;
+	point.setPosition(0,0);
+	point.setSize(sf::Vector2f(POINT_WIDTH, POINT_HEIGHT));
+	point.setFillColor(sf::Color::Red);
 }
 
 Point2D::Point2D(int x, int y) {
-	this->x = x;
-	this->y = y;
+	point.setPosition(x, y);
+	point.setSize(sf::Vector2f(POINT_WIDTH, POINT_HEIGHT));
+	point.setFillColor(sf::Color::Red);
 }
 
-Point2D::~Point2D() {}
+Point2D::Point2D(int x, int y, sf::Color color) {
+	point.setPosition(x, y);
+	point.setSize(sf::Vector2f(POINT_WIDTH, POINT_HEIGHT));
+	point.setFillColor(color);
+}
+
+
+void Point2D::draw(sf::RenderWindow* window) {
+	window->draw(point);
+}
+
+void Point2D::translate(sf::Vector2f offset) {
+	point.move(offset);
+}
+
+void Point2D::rotate(float angle) {
+	point.setRotation(angle);
+}
+
+void Point2D::scale(float factorX, float factorY) {
+	point.setScale(factorX, factorY);
+}
 
 int Point2D::getX() {
-	return this->x;
+	return point.getPosition().x;
 }
 int Point2D::getY() {
-	return this->y;
+	return point.getPosition().y;
+}
+
+void Point2D::setX(int x) {
+	point.setPosition(x, point.getPosition().y);
+}
+
+void Point2D::setY(int y) {
+	point.setPosition(point.getPosition().x, y);
+}
+
+sf::Color Point2D::getColor() {
+	return point.getFillColor();
+}
+void Point2D::setColor(sf::Color color) {
+	point.setFillColor(color);
 }
 
 void Point2D::setPoint(int x, int y) {
-	this->x = x;
-	this->y = y;
+	point.setPosition(x, y);
 }
 
-sf::RectangleShape Point2D::drawPoint() {
-	return primitiveRenderer->drawRectangle(x, y, 10, 10);
-}
+
 
 Point2D& Point2D::operator = (const Point2D& point) {
-	this->x = point.x;
-	this->y = point.y;
+	int newX = point.point.getPosition().x;
+	int newY = point.point.getPosition().y;
+	this->point.setPosition(newX, newY);
 	return *this;
 }
 
 bool Point2D::operator == (const Point2D& point) {
-	return this->getX() == point.x && this->getY() == point.y;
+
+	return this->getX() == point.point.getPosition().x && this->getY() == point.point.getPosition().y;
 }
--- 2D-Game/Point2D.h
+++ 2D-Game/Point2D.h
@@ -1,23 +1,37 @@
 #pragma once
 #include "Headers.h"
 #include "PrimitiveRenderer.h"
+#include "ShapeObject.h"
 
 class PrimitiveRenderer; // forward declaration
 
-class Point2D{
+class Point2D : public virtual ShapeObject{
 	private: 
-		int x, y;
+		static const int POINT_WIDTH = 10;
+		static const int POINT_HEIGHT = 10;
+
 		PrimitiveRenderer *primitiveRenderer;
+		sf::RectangleShape point;
 	public:
 		Point2D();
 		Point2D(int x, int y);
-
-		~Point2D();
+		Point2D(int x, int y, sf::Color color);
 
 		int getX();
 		int getY();
+		void setX(int x);
+		void setY(int y);
+
+		sf::Color getColor();
+		void setColor(sf::Color color);
+
 		void setPoint(int x, int y);
-		sf::RectangleShape drawPoint();
+
+		void draw(sf::RenderWindow* window);
+		void translate(sf::Vector2f offset);
+		void rotate(float angle);
+		void scale(float factorX, float factorY);
+
 
 		Point2D& operator= (const Point2D& point);
 		bool operator == (const Point2D& point);
