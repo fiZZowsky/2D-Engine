ab4ec0dcd32c8b4028d6f3a75d07e74938c6969f
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2022-11-20 10:23:42 +0100

    Dodanie metody usuwającej bitmape,  kopiującą, odbijającej bitmape w poziomie lub pionie

--- 2D-Game/BitmapHandler.cpp
+++ 2D-Game/BitmapHandler.cpp
@@ -6,10 +6,25 @@ sf::Image BitmapHandler::create(unsigned int width, unsigned int height, const s
 	return bitmap;
 }
 
+void BitmapHandler::remove(sf::Image* bitmap) {
+	bitmap = new sf::Image();
+}
+
 bool BitmapHandler::loadFromFile(sf::Image *bitmap, std::string filename) {
 	return bitmap -> loadFromFile(filename);
 }
 
 bool BitmapHandler::saveToFile(sf::Image bitmap, std::string filename) {
 	return bitmap.saveToFile(filename);
+}
+
+void BitmapHandler::copy(sf::Image* src, sf::Image *dst) {
+	dst->copy(*src, 0, 0);
+}
+
+void BitmapHandler::flipHorizontally(sf::Image* bitmap) {
+	bitmap->flipHorizontally();
+}
+void BitmapHandler::flipVertically(sf::Image* bitmap) {
+	bitmap->flipVertically();
 }
