fd55a2cde6c156b612af22df59e04caf4670b515
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2022-11-20 09:43:59 +0100

    Dodanie metod pozwalających na odczyt bitmapy z pliku i zapis bitmapy do pliku

--- 2D-Game/BitmapHandler.h
+++ 2D-Game/BitmapHandler.h
@@ -6,5 +6,10 @@ class BitmapHandler
 {
 public:
 	sf::Image create(unsigned int width, unsigned int height, const sf::Color color = sf::Color(0, 0, 0));
+	bool loadFromFile(sf::Image *bitmap, std::string filename);
+	bool saveToFile(sf::Image bitmap, std::string filename);
+	void copy();
+	void flipHorizontally();
+	void flipVertically();
 
 };
